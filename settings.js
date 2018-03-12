08ae248dfccc2afb95c1ce150409e8a3a902d0ef
NathanLeGeek <demoineretnathan@gmail.com>
2018-03-12 19:49:53 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 const ownerid = process.env.ownerid
 const modrolename = 'Mod'
 const adminrolename = 'Admin'
-const prefix = 'process.env.prefix'
+const prefix = process.env.prefix
 const token = process.env.token 
 
